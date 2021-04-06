16a5f8a44e9ca7876ab3b1ba64f26abb355de305
Nikhil Vispute <Vibhuti53>
2021-04-06 17:09:19 +0530

    constraint learnet

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,15 +37,17 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6 = new Log(230,180,80,PI/2);
+    slingshot = new Slingshot(bird.body, log6.body);
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+    //console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
+
     box1.display();
     box2.display();
     ground.display();
@@ -59,6 +63,8 @@ function draw(){
     log4.display();
     log5.display();
 
+    slingshot.display();
     bird.display();
+    log6.display();
     platform.display();
 }
